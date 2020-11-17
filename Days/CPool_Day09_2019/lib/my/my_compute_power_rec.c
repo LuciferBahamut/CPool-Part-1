562657db9d91b483a59a34677c85743ce61a3eed
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-11-17 16:45:35 +0100

    [ADD] day9

---
+++ Days/CPool_Day09_2019/lib/my/my_compute_power_rec.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** my_compute_power.c
+** File description:
+** task01
+*/
+
+int my_compute_power_rec(int nb, int power)
+{
+    return (0);
+}
---
+++ Days/CPool_Day09_2019/lib/my/my_strlowcase.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** *my_strlowcase.c
+** File description:
+** task01
+*/
+
+char *my_strlowcase(char *str)
+{
+    return (0);
+}
